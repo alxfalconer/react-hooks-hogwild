af995e8865261d15513284c21bf3f2f947c9c658
Alexander <al.x.falconer@gmail.com>
2022-06-27 14:00:00 +0100

    tile update

--- src/components/HogTile.js
+++ src/components/HogTile.js
@@ -13,4 +13,27 @@ class HogTile extends React.Component {
       let pigPics = require(`../hog-imgs/${formattedName}.jpg`);
       return pigPics;
     };
-}
+    handleDetailsClick = () => {
+        //when clicked, change clicked to true
+        this.setState({ clicked: !this.state.clicked });
+       };
+    
+       render() {
+         const { name, specialty } = this.props.hog;
+         return (
+           <div className="ui card eight wide column pigTile">
+               <button className="ui button" onClick={this.handleDetailsClick}>
+                 {this.state.clicked ? "Less Info" : "More Info"}
+               </button>
+               <button
+                 className="ui button"
+                 onClick={() => this.props.handleBanishedClick(this.props.hog)}
+               >
+                 Hide Me 🐽
+               </button>
+             </div>
+           
+         );
+        }
+    }
+    export default HogTile;
