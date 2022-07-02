f8fa792e0b6786cd1d19f26fb1bedd4efcab7fe3
Alexander <al.x.falconer@gmail.com>
2022-07-02 14:00:00 +0100

    filter 1st commit

---
+++ src/components/Filter.js
@@ -0,0 +1,23 @@
+const Filter = props => {
+    return (
+      <div className="filterWrapper">
+        <form>
+          <label>Sort By: </label>
+          <select
+            name="sort"
+            onChange={props.handleSelectChange}
+            value={props.sortBy}
+          >
+            <option value="name">Name</option>
+            <option value="weight">Weight</option>
+          </select>
+          <label>Greased Pigs Only?</label>
+          <input
+            checked={props.greased}
+            onChange={props.handleToggleGreased}
+            type="checkbox"
+          />
+        </form>
+        </div>
+   );
+ };
